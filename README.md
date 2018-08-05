133b308251dff7479755c247a20321f70cfca4d7
Copple <pcopplestone@gmail.com>
2018-08-05 12:04:06 +0800

    got a basline dev environment with watches on changes

---
+++ README.md
@@ -0,0 +1,16 @@
+# Kue Workers (by Pollygot)
+
+A KueJS implementation for common async tasks (written in Typescript). 
+
+Kue Workers has the following:
+
+- Facebook Bots
+- Mailgun
+- Slack
+- Telegram
+- Twilio
+
+
+## Development
+
+If you're using VS Code, you can use `cmd + shift + b` to run the default build task (which is mapped to npm run build), and then you can use the command palette (`cmd + shift + p`) and select `Tasks: Run Task > npm: start` to run npm start for you.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-const world = '🗺️';
+import * as express from 'express'
+import * as dotenv from "dotenv"
 
-export function hello(word: string = world): string {
-  return `Hello ${world}! `;
-}
+dotenv.config({ path: ".env.example" }) // Use this file as a baseline for your own environment
+const ExpressApp: express.Application = express()
+const REDIS_HOST: string = process.env.REDIS_HOST || '127.0.0.1'
+const EXPRESS_PORT: number = +(process.env.PORT || 3000)
+const KUE_PORT: number = +(process.env.PORT || 3001)
+
+ExpressApp.listen(EXPRESS_PORT, () => {
+  console.log('Kue interface on localhost:' + EXPRESS_PORT + '/kue/')
+  console.log('Kue JSON API on localhost:' + EXPRESS_PORT + '/api/')
+  console.log('Kue Instance on localhost:' + KUE_PORT)
+})
